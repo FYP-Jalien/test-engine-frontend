6b2d549736df63d4972168e3c0dc3d6ce421626d
Malith-19 <malith.19@cse.mrt.ac.lk>
2024-05-05 19:17:01 +0530

    Add test suite start & stop api calls.

--- src/pages/test-suite.tsx
+++ src/pages/test-suite.tsx
@@ -19,18 +19,34 @@ export type TestDetails = {
   constraint: string;
 };
 
-const handleStart = async () => {
-  try {
-    await api.get("/tests");
-    toast.info("Starting the test suite.");
-  } catch (error) {
-    console.log(error);
-    toast.error("Starting test suite faild.");
-  }
-};
-
 export default function TestSuite() {
   const [allTestDetails, setAllTestDetails] = useState<TestDetails[]>([]);
+  const [testSuitePID, setTestSuitePID] = useState(null);
+
+  // Starting thee test suite.
+  const handleStart = async () => {
+    try {
+      const response = await api.get("/tests/");
+      setTestSuitePID(response.data["pid"]);
+      toast.info("Starting the test suite.");
+    } catch (error) {
+      console.log(error);
+      toast.error("Starting test suite faild.");
+    }
+  };
+
+  const handleStop = async () => {
+    if (!testSuitePID) {
+      toast.info("Test suite is not running at the moment.");
+      return;
+    }
+    try {
+      await api.get(`tests/stop/${testSuitePID}`);
+      toast.success("Successfully killed the test suite process.");
+    } catch (error) {
+      console.log("Errors stoping the test suite", error);
+    }
+  };
 
   useEffect(() => {
     setAllTestDetails([
@@ -63,12 +79,7 @@ export default function TestSuite() {
           </IconButton>
         </Tooltip>
         <Tooltip title="Stop the test process">
-          <IconButton
-            disabled={true}
-            onClick={() => {
-              console.log("disabled");
-            }}
-          >
+          <IconButton onClick={handleStop}>
             <StopCircleIcon fontSize="large" color="error" />
           </IconButton>
         </Tooltip>
